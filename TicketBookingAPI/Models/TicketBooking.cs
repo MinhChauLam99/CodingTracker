612a20a765ba4316ceccf1e86e240055973d105d
MinChou <Minhchausaleslogs99@gmail.com>
2023-08-28 19:44:01 +0700

    create a new project

--- TicketBookingAPI/Models/TicketBooking.cs
+++ TicketBookingAPI/Models/TicketBooking.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public int TicketNumber { get; set; }
         public string? ClientName { get; set; }
+        public int rank { get; set; }
+
     }
 }
